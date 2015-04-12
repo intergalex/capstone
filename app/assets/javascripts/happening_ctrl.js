2279dd3ab638814099140489828eaf3c25dfccbb
Alexander Cole <alexanderthecole@gmail.com>
2015-04-12 16:10:37 -0500

    angular clickable form added

--- app/assets/javascripts/happening_ctrl.js
+++ app/assets/javascripts/happening_ctrl.js
@@ -18,25 +18,26 @@
   $scope.makeTrust = function(html){
         return $sce.trustAsHtml(html);
       };
-  // $scope.addHappening = function(newFirstName, newLastName, newBio) {
-  //   var happening = {
-  //     name: newName,
-  //     address: newAddress,
-  //     description: newDescription
-  //     human_time: newStartTime 
-  //   };
 
-  //   $http.post('api/v1/happenings.json', happeing).then(function(response) {
-  //       $scope.happenings.push(character);
-  //       $scope.newName = null;
-  //       $scope.newAddress = null;
-  //       $scope.newDescription = null;
-  //       $scope.newStartTime = null;
-  //     }, function(error) {
-  //       $scope.errors = error.data.errors;
-  //     });  
+  $scope.addHappening = function(newFirstName, newLastName, newBio) {
+    var happening = {
+      name: newName,
+      address: newAddress,
+      description: newDescription,
+      human_time: newStartTime 
+    };
+
+    $http.post('api/v1/happenings.json', happeing).then(function(response) {
+        $scope.happenings.push(character);
+        $scope.newName = null;
+        $scope.newAddress = null;
+        $scope.newDescription = null;
+        $scope.newStartTime = null;
+      }, function(error) {
+        $scope.errors = error.data.errors;
+      });  
   
-  // };
+  };
 
   window.scope = $scope; 
 
--- app/models/user_happening.rb
+++ app/models/user_happening.rb
@@ -1,4 +1,6 @@
 class UserHappening < ActiveRecord::Base
 
+  belongs_to :user 
+
   
 end
